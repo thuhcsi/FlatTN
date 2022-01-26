743ece120b7006f1f62ea8915d4cceadbbdcb3b2
zywu <johnson.tsing@gmail.com>
2022-01-26 22:00:28 +0800

    Refine the data process code

--- dataset/processed/preprocess.py
+++ dataset/processed/preprocess.py
@@ -2,34 +2,45 @@ import pandas as pd
 import codecs
 import json
 
-path_name=r"./CN_TN_epoch-01-28645_2.jsonl"
 
-#out_name=r"C:\Edge\CN_TN_epoch-01\tn.jsonl"
-#tn_BMES=r"C:\Edge\CN_TN_epoch-01\tn_BMES.txt"
-#tns=open(tn_BMES,'w')
-#out_f=open(out_name,'w')
+def process_word(word, label):
+    st = ''
+    if (len(word)==1):
+        st = st + word + ' ' + "S-"+label[2] + '\n'
+    else:
+        index = 0
+        for char in word:
+            if (index==0):
+                st = st + char + ' ' + "B-"+label[2]+ '\n'
+            elif (index<(len(word)-1)):
+                st = st + char + ' ' + "M-"+label[2] + '\n'
+            elif (index==(len(word)-1)):
+                st = st + char + ' ' + "E-"+label[2] + '\n'
+            index+=1
+    return st
 
-p1=r"./shuffled_BMES"
-t1=p1+'/train.char.bmes'
-t2=p1+'/dev.char.bmes'
-t3=p1+'/test.char.bmes'
 
-tt1 = codecs.open(t1, 'w', 'utf-8')
-tt2 = codecs.open(t2, 'w', 'utf-8')
-tt3 = codecs.open(t3, 'w', 'utf-8')
+raw_json = r"./CN_TN_epoch-01-28645_2.jsonl"
+
+out_path = r"./shuffled_BMES"
+train = out_path + '/train.char.bmes'
+dev   = out_path + '/dev.char.bmes'
+test  = out_path + '/test.char.bmes'
+
+ftrain = codecs.open(train, 'w', 'utf-8')
+fdev   = codecs.open(dev,   'w', 'utf-8')
+ftest  = codecs.open(test,  'w', 'utf-8')
 
 import random
 
-#bb1=open(r"C:\Edge\CN_TN_epoch-01\tn_rule_data.txt",'w')
-#bb2=open(r"C:\Edge\CN_TN_epoch-01\CN_TN_epoch-01\CN_TN_epoch-02-1355_3.jsonl",'w')
-
+# load data
+print("Loading data...")
 dd=[]
-with open(path_name, 'r') as load_f:
+with open(raw_json, 'r') as load_f:
     for l in load_f.readlines():
-        dic=json.loads(l)
-        #l.encode('utf-8').decode('unicode_escape')
-        j=json.dumps(dic, ensure_ascii=False)
-        d=json.loads(j)
+        dic = json.loads(l)
+        j = json.dumps(dic, ensure_ascii=False)
+        d = json.loads(j)
 
         text = d["text"]
         # print('CEHCK text from d:', d["text"])
@@ -39,104 +50,41 @@ with open(path_name, 'r') as load_f:
         # print('CEHCK labels length from d:', d["labels"])
         d["id"] = d["id"]
         # print('CEHCK id length from d:', d["id"])
-        #json.dump(d,bb2)
-        #bb2.write('\n')
         dd.append(d)
 
-# for se in dd:
-#     text=se["text"]
-#     le=len(text)
-#     labels=se["labels"]
-#     for label in labels:
-#             word=text[label[0]:label[1]]
-#             st=word+' '+"S-"+label[2]+'\n'
-#             tt1.write(st)
-#             if(label[1]==le):
-                
-#                 tt1.write('\n')     
-
-
-# setting the shuffle to get the same train/test/dev segmentation
+# set the random seed for shuffle to get the same train/dev/test segmentation
 random.Random(30).shuffle(dd)
 
+num_items = len(dd)
+train_end = int(num_items * 0.8)
+dev_end   = int(num_items * 0.9)
+print(num_items, train_end, dev_end)
 
+# write processed data 
 id=0
 for se in dd:
-    text=se["text"]
-    le=len(text)
-    labels=se["labels"]
-    id+=1
-   
-#得到BMES的标签
-    if(id>=0 and id<24000):
-        for label in labels:
-            word=text[label[0]:label[1]]
-            if(len(word)==1):
-                st=word+' '+"S-"+label[2]+'\n'
-                tt1.write(st)
-                if(label[1]==le):
-                    tt1.write('\n')
-            else:
-                index=0
-                for char in word:
-                    if(index==0):
-                        st=char+' '+"B-"+label[2]+'\n'
-                    if(index>0 and index<(len(word)-1)):
-                        st=char+' '+"M-"+label[2]+'\n'
-                    if(index==(len(word)-1)):
-                        st=char+' '+"E-"+label[2]+'\n'
-                    tt1.write(st)
-                    index+=1
-    
-    if(id>=24000 and id<27000):
-        for label in labels:
-            word=text[label[0]:label[1]]
-            if(len(word)==1):
-                st=word+' '+"S-"+label[2]+'\n'
-                tt2.write(st)
-                if(label[1]==le):
-                    tt2.write('\n')
-            else:
-                index=0
-                for char in word:
-                    if(index==0):
-                        st=char+' '+"B-"+label[2]+'\n'
-                    if(index>0 and index<(len(word)-1)):
-                        st=char+' '+"M-"+label[2]+'\n'
-                    if(index==(len(word)-1)):
-                        st=char+' '+"E-"+label[2]+'\n'
-                    tt2.write(st)
-                    index+=1
+    text   = se["text"]
+    le     = len(text)
+    labels = se["labels"]
+    id += 1
 
-    if(id>=27000 and id<30000):
-        # print('CHECK test set HERE!!!')
-        for label in labels:
-            word=text[label[0]:label[1]]
-            if(len(word)==1):
-                st=word+' '+"S-"+label[2]+'\n'
-                tt3.write(st)
-                if(label[1]==le):
-                    tt3.write('\n')
-            else:
-                index=0
-                for char in word:
-                    if(index==0):
-                        st=char+' '+"B-"+label[2]+'\n'
-                    if(index>0 and index<(len(word)-1)):
-                        st=char+' '+"M-"+label[2]+'\n'
-                    if(index==(len(word)-1)):
-                        st=char+' '+"E-"+label[2]+'\n'
-                    tt3.write(st)
-                    index+=1
+    # get BMES labels
+    for label in labels:
+        word = text[label[0]:label[1]]
+        st = process_word(word, label)
+
+        # write to different file
+        if (id <= train_end):
+            ftrain.write(st)
+            if (label[1] == le):
+                ftrain.write('\n')
+        elif (id <= dev_end):
+            fdev.write(st)
+            if (label[1] == le):
+                fdev.write('\n')
+        else:
+            ftest.write(st)
+            if (label[1] == le):
+                ftest.write('\n')
 
 print(id)
-        
-        
-
-
-              
-
-                
-            
-
-        
--- dataset/processed/stat.py
+++ dataset/processed/stat.py
@@ -1,67 +1,49 @@
 
 from os import write
 
+#这里dict是跑数据集收集到的
+dict = {'SELF': 0, 'PUNC': 0, 'ENG_LETTER': 0, 'VERBATIM': 0, 'DIGIT': 0, 'HYPHEN_RANGE': 0, 'CARDINAL': 0, 'HYPHEN_RATIO': 0, 'MEASURE_UNIT': 0, 'POINT': 0,
+ 'HYPHEN_IGNORE': 0, 'NUM_ENG': 0, 'SLASH_YEAR': 0, 'SLASH_MONTH': 0, 'DAY_CARDINAL': 0, 'ABBR': 0, 'COLON_HOUR': 0, 'MINUTE_CARDINAL': 0, 'SLASH_PER': 0,
+ 'NUM_TWO_LIANG': 0, 'SLASH_FRACTION': 0, 'SLASH_OR': 0, 'COLON_MINUTE': 0, 'SECOND_CARDINAL': 0, 'HYPHEN_SUBZERO': 0, 'HYPHEN_MINUS': 0, 'MONTH_CARDINAL': 0,}
 
-a=open(r"C:\Edge\CN_TN_epoch-01\Shufffled_BMES\test.char.bmes",'r',encoding='ANSI')
-#这里dic是跑数据集收集到的
-dic={'SELF':0,'PUNC': 0, 'ENG_LETTER': 0, 'VERBATIM': 0, 'DIGIT': 0, 'HYPHEN_RANGE': 0, 'CARDINAL': 0, 'HYPHEN_RATIO': 0, 'MEASURE_UNIT': 0, 'POINT': 0.012, 'HYPHEN_IGNORE': 
-0.003, 'NUM_ENG': 0.0, 'SLASH_YEAR': 0.0, 'SLASH_MONTH': 0.0, 'DAY_CARDINAL': 0.0, 'ABBR': 0.0, 'COLON_HOUR': 0.0, 'MINUTE_CARDINAL': 0.0, 'SLASH_PER': 0.0, 'NUM_TWO_LIANG': 0.0, 'SLASH_FRACTION': 0.0, 'SLASH_OR':0,'COLON_MINUTE': 0.0, 'SECOND_CARDINAL': 0.0, 'HYPHEN_SUBZERO': 0.0,'HYPHEN_MINUS':0,'MONTH_CARDINAL':0,}
+def get_statistics(file, dic):
+    for k in dic.keys():
+        dic[k] = [0,0]
 
+    k = 0
+    a = open(file, 'r', encoding='utf-8')
+    for l in a.readlines():
+        if (l == '\n'):
+            pass
+        else:
+            l = l.strip('\n')
+            li= l.split(' ')
+            if (li[1] != 'O'):
+                li[1] = str(li[1]).split('-')[1]
+            if (li[1] in dic.keys()):
+                dic[li[1]][0] += 1
+                k += 1
 
-for k in dic.keys():
-    dic[k]=[0,0]
+    k = k-dic['SELF'][0]
+    k = k-dic['PUNC'][0]
+    del dic['SELF']
+    del dic['PUNC']
 
-k=0
-n=0
-for l in a.readlines():
-    
-    if(l=='\n'):
-        pass
-        n+=1
+    return (k, dic)
 
-    else:
-        
-        l=l.strip('\n')
-        li=l.split(' ')
-        if(li[1]!='O'):
-            st=str(li[1])
-            st=st.split('-')
-            li[1]=st[1]
-        if(li[1] in dic.keys()):
-            dic[li[1]][0]=dic[li[1]][0]+1
-            k+=1
-        
-k=k-dic['SELF'][0]
-k=k-dic['PUNC'][0]
-del dic['SELF']
-del dic['PUNC']
-print(k)
-my_tuple = zip(dic.values(), dic.keys())
-for v,key in my_tuple:
-    v[1]=v[0]/k
-    v[1]=round(v[1],6)
+file = r"./shuffled_BMES/test.char.bmes"
+(total, dic) = get_statistics(file, dict)
+print(total)
+
+for v,key in zip(dic.values(), dic.keys()):
+    v[1] = round(v[0]/total,6)
     v[1] = format(v[1], '.4%')
-    dic[key][1]=v[1]
-#b1=open(r"C:\Edge\CN_TN_epoch-01\dataset\tj.csv",'w+')
-"""
-for v,key in my_tuple:
-    l=key+','+v[0]+','+v[1]+'\n'
-    print(l)
-    b1.write(l)
-print(my_tuple)
-#print(sorted(my_tuple,reverse=True))
-"""
 print(dic)
 print(len(dic))
 
-b=r"C:\Edge\CN_TN_epoch-01\Shufffled_BMES\train.csv"
+stat = r"statistics.csv"
 
-b1=open(b,'a+')
+fstat = open(stat,'a+')
 for v,k in zip(dic.values(), dic.keys()):
-    l=k+','+str(v[0])+','+str(v[1])+','+'\n'
-    b1.write(l) 
-
-
-
-
-
+    l = k+','+str(v[0])+','+str(v[1])+','+'\n'
+    fstat.write(l) 
